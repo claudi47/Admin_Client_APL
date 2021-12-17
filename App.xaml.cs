3193419a78f0c7092ae8484b6e9bb2d860c42e6d
Claudio Sicari <claudiosicari99@gmail.com>
2021-12-17 18:52:31 +0100

    First commit v0.0

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Admin_Client_APL
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
